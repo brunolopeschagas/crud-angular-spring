a3a867e6c9c217f6f045f009b3865c5084f0558f
brunolopeschagas <brunolopeschagas@gmail.com>
2022-08-04 12:11:52 -0300

    CRUD Angular + Spring | 10 - Lista de Cursos: Spinner (Carregando)

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay, first } from 'rxjs';
 
 import { Course } from '../models/course';
 
@@ -13,6 +14,10 @@ export class CoursesService {
   constructor(private httpClient: HttpClient) { }
 
   list() {
-    return this.httpClient.get<Course[]>(this.API);
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      delay(15000)
+    );
   }
 }
--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
     MatToolbarModule,
     MatCardModule,
-    MatTableModule
+    MatTableModule,
+    MatProgressSpinnerModule
   ]
 })
 export class AppMaterialModule { }
