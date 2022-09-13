4a0d4adce164c3e0edba02500deff107d439fe10
Bruno <bruno.chagas@gmail.com>
2022-09-12 21:11:48 -0300

    CRUD Angular + Spring | 18: Componente de Formulário e Roteamento para criar cursos

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { catchError, of } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
@@ -15,19 +16,26 @@ import { CoursesService } from '../services/courses.service';
 export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]>;//$ - padrao para observables
-  displayedColumns = ['id', 'name', 'category'];
+  displayedColumns = ['id', 'name', 'category', 'actions'];
 
   constructor(
     private coursesService: CoursesService,
-    private dialog: MatDialog
-    ) {
+    private dialog: MatDialog,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
     this.courses$ = this.coursesService.list()
-    .pipe(
-      catchError(error => {
-        this.onError('Erro ao carregar cursos.');
-        return of([])
-      })
-    );
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar cursos.');
+          return of([])
+        })
+      );
+  }
+
+  onAdd() {
+    // this.router.navigate(['courses/new']);
+    this.router.navigate(['new'], {relativeTo: this.activatedRoute});
   }
 
   onError(errorMsg: string) {
