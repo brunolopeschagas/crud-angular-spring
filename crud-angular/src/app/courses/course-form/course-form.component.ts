5bdb77734430ab033e77672ed789fea520ba0fd0
Bruno <bruno.chagas@gmail.com>
2022-09-20 20:55:40 -0300

    CRUD Angular + Spring | 19: Formulário para Criar Cursos

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
@@ -6,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-form.component.scss']
 })
 export class CourseFormComponent implements OnInit {
+  
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
+  onCancel() {
+    throw new Error('Method not implemented.');
+  }
 
-  constructor() { }
+  form: FormGroup; //mesma variavel do course-form.component.html
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: [null], //mesma variavel do course-form.component.html
+      category: [null]//mesma variavel do course-form.component.html
+    });
+  }
 
   ngOnInit(): void {
   }
