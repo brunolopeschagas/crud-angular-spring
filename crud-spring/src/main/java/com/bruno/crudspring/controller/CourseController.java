3a5766d3cc269c3d1eb61dd92353435c725926dd
Bruno Lopes Chagas <bruno.chagas@sisema.net>
2022-12-05 12:33:25 -0300

    CRUD Angular + Spring | 26: API Spring: Buscar por ID

--- crud-spring/src/main/java/com/bruno/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/bruno/crudspring/controller/CourseController.java
@@ -2,9 +2,11 @@ package com.bruno.crudspring.controller;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +21,23 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/api/courses")
 @AllArgsConstructor
 public class CourseController {
-    
+
     private final CourseRepository courseRepository;
 
     @GetMapping
-    public List<Course> list(){
+    public List<Course> list() {
         return this.courseRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return this.courseRepository.findById(id)
+                .map(courseFound -> ResponseEntity.ok().body(courseFound))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @PostMapping
-    public ResponseEntity<Course> create(@RequestBody Course course){
+    public ResponseEntity<Course> create(@RequestBody Course course) {
         Course savedCourse = this.courseRepository.save(course);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedCourse);
     }
